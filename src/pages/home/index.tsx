23424846584953fd70e8190bd35c408b28340ecf
cherartem <artem138927@gmail.com>
2023-07-13 12:12:02 +0200

    Set up axios instance

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import ListOfArticles from "./components/ListOfArticles";
 
 export default function HomePage() {
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <Header />
+      <ListOfArticles />
     </div>
   );
 }
